449490fac21199262b34f58d28f01b91ade9edf9
Ochieng Brian <ochiengbrian25@yahoo.com>
2022-04-02 00:07:10 +0300

    new project, react bootsrap

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container'>
+        <div className='card shadow'>
+            <div className='card-body'>
+                <h2>Home</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
